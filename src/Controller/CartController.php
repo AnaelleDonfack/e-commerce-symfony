b44efe8701f76d94564603a9262d4810e0bf0350
Anaelle <anaellejeagni@gmail.com>
2021-11-10 15:52:47 +0100

    Mise en place de la mécanique de panier : ajout, suppression produits

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,19 +3,28 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     #[Route('/mon-panier', name: 'cart')]
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+
+        return $this->render('cart/index.html.twig',[
+            'cart' => $cart->getFull(),
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
@@ -26,10 +35,25 @@ class CartController extends AbstractController
         return $this->redirectToRoute('cart');
     }
 
-    #[Route('/cart/remove/{id}', name: 'remove_to_cart')]
+    #[Route('/cart/delete/{id}', name: 'delete_to_cart')]
+    public function delete(Cart $cart, $id){
+
+        $cart->delete($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/cart/remove/', name: 'remove_to_cart')]
     public function remove(Cart $cart){
 
         $cart->remove();
         return $this->redirectToRoute('products');
+    }
+
+    #[Route('/cart/decrease/{id}' , name: 'decrease_to_cart')]
+    public function decrease(Cart $cart, $id){
+        $cart->decrease($id);
+
+        return $this->redirectToRoute('cart');
     }
 }
--- src/Controller/Classe/Cart.php
+++ src/Controller/Classe/Cart.php
@@ -2,14 +2,46 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
+/**
+ * Class Cart
+ * @package App\Classe
+ * Contenant toute la mécanique concernant le panier : ajout, suppression
+ */
 class Cart{
 
     private $requestStack;
-    public function __construct(RequestStack $requestStack)
+    private $entityManager;
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager)
     {
         $this->requestStack = $requestStack;
+        $this->entityManager = $entityManager;
+
+    }
+
+    public function getFull(){
+
+        $cartComplete = [];
+        if($this->get()) {
+            foreach ($this->get() as $id => $quantity) {
+                $product = $this->entityManager->getRepository(Product::class)->findOneBy(['id' => $id]);
+                if($product){
+                    $cartComplete[] = [
+                        'product' => $product,
+                    'quantity' => $quantity,
+                ];
+                }else{
+                    $this->delete($id);
+                    continue;
+                }
+            }
+        }
+
+        return $cartComplete;
+
     }
 
     //Fonction qui permet d'ajout un produit au panier
@@ -22,10 +54,29 @@ class Cart{
         //SI le produit d'id spécifique existe deja tu incrémentes
         if(!empty($cart[$id])){
             $cart[$id]++;
+
         }else{
             $cart[$id] = 1;
+
         }
-        //Cree une session panier auquel tu associes un tableau[id, quantity]
+        //Cree une session panier:cart auquel tu associes un tableau[id, quantity]
+        $session->set('cart', $cart);
+    }
+
+    public function decrease($id){
+
+        $session = $this->requestStack->getSession();
+        //Tu me renvoies une session sous forme de tableau
+        $cart = $session->get('cart',[]);
+
+        //Vérifier si la quantité de notre produit > 1
+        if($cart[$id] > 1){
+            $cart[$id]--;
+        }else{
+            unset($cart[$id]);
+        }
+
+        //Cree une session panier:cart auquel tu associes un tableau[id, quantity]
         $session->set('cart', $cart);
     }
 
@@ -36,5 +87,14 @@ class Cart{
 
     public function remove(){
         return $this->requestStack->getSession()->remove('cart');
+    }
+
+    public function delete($id){
+        $cart = $this->requestStack->getSession()->get('cart');
+
+        //Retire du tableau cart celui qui a l'id passé en paramètre
+         unset($cart[$id]);
+
+         return $this->requestStack->getSession()->set('cart', $cart);
     }
 }
